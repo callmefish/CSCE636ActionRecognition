1d570ae8fab27bc3bf6971d8494adbd063c84c3d
Zhiyu Yan <31463079+callmefish@users.noreply.github.com>
2020-03-23 20:27:39 -0500

    Add files via upload

---
+++ save_pickle.py
@@ -0,0 +1,16 @@
+import pickle
+import os
+
+first_dir = 'video_data/'
+first_dir_sub = os.listdir(first_dir)
+
+frame_count = {}
+
+for i in first_dir_sub:
+    number = len(os.listdir(first_dir + i))
+    frame_count[i] = number
+
+
+pickle_file = open('dataloader/dic/frame_count.pickle', 'wb')
+pickle.dump(frame_count, pickle_file)
+pickle_file.close()
