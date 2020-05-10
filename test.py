41fca5c3922bfa5f867556a826444418a79ab6a8
Zhiyu Yan <31463079+callmefish@users.noreply.github.com>
2020-05-09 19:06:31 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import cv2
 import numpy as np
-import pickle
 import os
 import time
 from tqdm import tqdm
 import shutil
-from random import randint
 import argparse
 from glob import glob
 import torchvision.transforms as transforms
@@ -13,41 +11,36 @@ import torchvision.models as models
 import torch.nn as nn
 import torch
 import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from test_spatial_dataloader import *
 from test_motion_dataloader import *
 from utils import *
 from network import *
-import math
 import json
 import matplotlib.pyplot as plt
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# 添加命令行指令
 parser = argparse.ArgumentParser(description='video test for two stream')
 parser.add_argument('--batch-size', default=19, type=int, metavar='N', help='mini-batch size (default: 25)')
 parser.add_argument('--lr', default=5e-4, type=float, metavar='LR', help='initial learning rate')
-parser.add_argument('--video_name', type=str, default='csce636test/6700_2.mp4')
+parser.add_argument('--video_name', type=str, default='csce636test/6000_4.mp4')
 arg = parser.parse_args()
 rgb_whole_pred = {}
 opf_whole_pred = {}
+
 
 def main(start_frame):
     # spatial Prepare DataLoader
     spatial_data_loader = test_spatial_dataloader(
         BATCH_SIZE=arg.batch_size,
         num_workers=8,
-        path='record/temp_chunk/',
+        path='record/test/temp_chunk/',
     )
     spatial_test_loader = spatial_data_loader.run()
     spatial_model = MODEL(
         lr=arg.lr,
         batch_size=arg.batch_size,
-        # resume='best_model_475/spatial/model_best.pth.tar',
         resume='555_rgb_model_best.pth.tar',
-        start_epoch=0,
         evaluate='evaluate',
         test_loader=spatial_test_loader,
         channel=3,
@@ -60,14 +53,12 @@ def main(start_frame):
         BATCH_SIZE=arg.batch_size,
         num_workers=8,
         in_channel=10,
-        path='record/temp_opf/'
+        path='record/test/temp_opf/'
     )
     motion_test_loader = motion_data_loader.run()
     motion_model = MODEL(
         test_loader=motion_test_loader,
-        start_epoch=0,
         resume='475_opt_model_best.pth.tar',
-        # resume='555_opt_model_best.pth.tar',
         evaluate='evaluate',
         lr=arg.lr,
         batch_size=arg.batch_size,
@@ -78,12 +69,11 @@ def main(start_frame):
 
 
 class MODEL():
-    def __init__(self, lr, batch_size, resume, start_epoch, evaluate, test_loader, channel,
+    def __init__(self, lr, batch_size, resume, evaluate, test_loader, channel,
                  start_frame):
         self.lr = lr
         self.batch_size = batch_size
         self.resume = resume
-        self.start_epoch = start_epoch
         self.evaluate = evaluate
         self.test_loader = test_loader
         self.best_prec1 = 0
@@ -92,18 +82,12 @@ class MODEL():
 
     def build_model(self):
         self.model = resnet101(pretrained=True, channel=self.channel).cuda()
-        self.criterion = nn.CrossEntropyLoss().cuda()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), self.lr, momentum=0.9)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', patience=1, verbose=True)
 
     def resume_and_evaluate(self):
         if self.resume:
             if os.path.isfile(self.resume):
                 checkpoint = torch.load(self.resume)
-                self.start_epoch = checkpoint['epoch']
-                self.best_prec1 = checkpoint['best_prec1']
                 self.model.load_state_dict(checkpoint['state_dict'])
-                self.optimizer.load_state_dict(checkpoint['optimizer'])
             else:
                 print("==> no checkpoint found at '{}'".format(self.resume))
         if self.evaluate:
@@ -146,11 +130,10 @@ def cal_for_frames(video_path, video_name, flow_path):
         prev = curr
         if not os.path.exists(os.path.join(flow_path, video_name + '_u')):
             os.mkdir(os.path.join(flow_path, video_name + '_u'))
-        cv2.imwrite(os.path.join(flow_path, video_name + '_u', "{:06d}.jpg".format(i+1)), tmp_flow[:, :, 0])
+        cv2.imwrite(os.path.join(flow_path, video_name + '_u', "{:06d}.jpg".format(i + 1)), tmp_flow[:, :, 0])
         if not os.path.exists(os.path.join(flow_path, video_name + '_v')):
             os.mkdir(os.path.join(flow_path, video_name + '_v'))
-        cv2.imwrite(os.path.join(flow_path, video_name + '_v', "{:06d}.jpg".format(i+1)), tmp_flow[:, :, 1])
-
+        cv2.imwrite(os.path.join(flow_path, video_name + '_v', "{:06d}.jpg".format(i + 1)), tmp_flow[:, :, 1])
     return
 
 
@@ -160,7 +143,6 @@ def compute_TVL1(prev, curr, bound=15):
     flow = TVL1.calc(prev, curr, None)
     flow = cv2.UMat.get(flow)
     assert flow.dtype == np.float32
-
     flow = (flow + bound) * (255.0 / (2 * bound))
     flow = np.round(flow).astype(int)
     flow[flow >= 255] = 255
@@ -173,46 +155,64 @@ def extract_flow(video_path, video_name, flow_path):
     print('complete:' + flow_path + video_name)
     return
 
+
 def softmax(data):
     data_exp = np.exp(data)
     return data_exp / np.sum(data_exp)
 
 
+def save_fig(x, y, title, save_path):
+    plt.figure()
+    plt.plot(x, y, linewidth=2, color='lightskyblue')
+    plt.xlabel('time/s')
+    plt.ylabel('Slipping probability')
+    plt.ylim(0, 1.1)
+    plt.xlim(0, duration + 0.2)
+    plt.title(title)
+    plt.savefig(save_path)
+    # plt.show()
+
+def revise_order(path, isRGB):
+    path_sub = os.listdir(path)
+    path_sub.sort()
+    if isRGB:
+        for j in range(len(path_sub)):
+            old_name = path + path_sub[j]
+            new_name = path + 'frame_' + str(j+1).zfill(6) + '.jpg'
+            os.rename(old_name, new_name)
+    else:
+        for j in range(len(path_sub)):
+            old_name = path + path_sub[j]
+            new_name = path + str(j+1).zfill(6) + '.jpg'
+            os.rename(old_name, new_name)
+
+def make_sure_path(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
 if __name__ == '__main__':
+    make_sure_path('record/')
     file_path = arg.video_name
     video_title = file_path.split('/')[-1][:-4]
     print(video_title)
     cap = cv2.VideoCapture(file_path)
-    if cap.isOpened():  
-        rate = cap.get(5)  
-        FrameNumber = cap.get(7)  
+    if cap.isOpened():
+        rate = cap.get(5)
+        FrameNumber = cap.get(7)
         duration = FrameNumber / rate
         width = cap.get(3)
-        height = cap.get(4)  
+        height = cap.get(4)
         print(duration)
     rgb_outPutDirName = 'record/temp_chunk/'
     opf_outPutDirName = 'record/temp_opf/'
+    make_sure_path(rgb_outPutDirName)
+    make_sure_path(opf_outPutDirName)
 
-    if not os.path.exists(rgb_outPutDirName):
-        os.makedirs(rgb_outPutDirName)
-    if not os.path.exists(opf_outPutDirName):
-        os.makedirs(opf_outPutDirName)
-
-    frame = 1
     index = 0
-    
-
     while True:
         res, image = cap.read()
         if not res:
             print('not res , not image')
-            if index > 27:
-                extract_flow(rgb_outPutDirName, 'v_temp_opf', opf_outPutDirName)
-                StartFrame = frame - len(os.listdir(rgb_outPutDirName))
-                main(StartFrame)
-            
-            shutil.rmtree(rgb_outPutDirName)
-            shutil.rmtree(opf_outPutDirName)    
             break
         else:
             if width < height:
@@ -220,30 +220,52 @@ if __name__ == '__main__':
                 image = cv2.copyMakeBorder(image, 0, 0, pad, pad, cv2.BORDER_CONSTANT, value=0)
             image = cv2.resize(image, (342, 256))
             cv2.imwrite(rgb_outPutDirName + 'frame_' + str(index + 1).zfill(6) + '.jpg', image)
-            frame += 1
             index += 1
-            if (frame - 1) % 30 == 0:
-                extract_flow(rgb_outPutDirName, 'v_temp_opf', opf_outPutDirName)
-                StartFrame = frame - 31
-                main(StartFrame)
-                # break
-                shutil.rmtree(rgb_outPutDirName)
-                os.makedirs(rgb_outPutDirName)
-                shutil.rmtree(opf_outPutDirName)
-                os.makedirs(opf_outPutDirName)
-                index = 0
-
-    print('finish video analysis')
+    print('extract rgb finished')
+    extract_flow(rgb_outPutDirName, 'v_temp_opf', opf_outPutDirName)
     cap.release()
     time_lable = {}
 
-    fig_x = []
-    fig_y = []
-    fig_y_rgb = []
-    fig_y_opf = []
+    make_sure_path('record/test/')
+    make_sure_path('record/test/temp_chunk/')
+    make_sure_path('record/test/temp_opf/')
+    make_sure_path('record/test/temp_opf/v_temp_opf_u/')
+    make_sure_path('record/test/temp_opf/v_temp_opf_v/')
+    frame = 1
+    for i in range(1, 41):
+        shutil.copyfile(rgb_outPutDirName + 'frame_' + "{:06d}.jpg".format(i),
+                        'record/test/temp_chunk/' + 'frame_' + "{:06d}.jpg".format(i))
+        shutil.copyfile(opf_outPutDirName + 'v_temp_opf_u/' + "{:06d}.jpg".format(i),
+                        'record/test/temp_opf/v_temp_opf_u/' + "{:06d}.jpg".format(i))
+        shutil.copyfile(opf_outPutDirName + 'v_temp_opf_v/' + "{:06d}.jpg".format(i),
+                        'record/test/temp_opf/v_temp_opf_v/' + "{:06d}.jpg".format(i))
+    frame = 41
+    StartFrame = frame - 41
+    main(StartFrame)
+
+    while frame + 10 < index:
+        for i in range(1, 11):
+            os.remove('record/test/temp_chunk/' + 'frame_' + "{:06d}.jpg".format(i))
+            shutil.copyfile(rgb_outPutDirName + 'frame_' + "{:06d}.jpg".format(frame),
+                            'record/test/temp_chunk/' + 'frame_' + "{:06d}.jpg".format(frame))
+            os.remove('record/test/temp_opf/v_temp_opf_u/' + "{:06d}.jpg".format(i))
+            shutil.copyfile(opf_outPutDirName + 'v_temp_opf_u/' + "{:06d}.jpg".format(frame),
+                            'record/test/temp_opf/v_temp_opf_u/' + "{:06d}.jpg".format(frame))
+            os.remove('record/test/temp_opf/v_temp_opf_v/' + "{:06d}.jpg".format(i))
+            shutil.copyfile(opf_outPutDirName + 'v_temp_opf_v/' + "{:06d}.jpg".format(frame),
+                            'record/test/temp_opf/v_temp_opf_v/' + "{:06d}.jpg".format(frame))
+            frame += 1
+        revise_order('record/test/temp_chunk/', True)
+        revise_order('record/test/temp_opf/v_temp_opf_u/', False)
+        revise_order('record/test/temp_opf/v_temp_opf_v/', False)
+        StartFrame += 10
+        main(StartFrame)
+
+
+    fig_x, fig_y, fig_y_rgb, fig_y_opf = [], [], [], []
     for key in list(rgb_whole_pred.keys()):
-        cur_time = float(key)/rate
-        new_key = str(float('%.3f'%cur_time))
+        cur_time = float(key) / rate
+        new_key = str(float('%.3f' % cur_time))
         new_value = softmax(rgb_whole_pred[key] + 1 * opf_whole_pred[key]).tolist()
         rgb_value = softmax(rgb_whole_pred[key]).tolist()
         opf_value = softmax(opf_whole_pred[key]).tolist()
@@ -252,16 +274,31 @@ if __name__ == '__main__':
         fig_y.append(new_value[0])
         fig_y_rgb.append(rgb_value[0])
         fig_y_opf.append(opf_value[0])
-    
 
+    point_num = len(fig_y_rgb)
+    one_count = 0
+    zero_count = 0
+    for i in fig_y_rgb:
+        if abs(i-1) < 1e-4:
+            one_count += 1
+        if abs(i-0) < 1e-4:
+            zero_count += 1
+    if one_count/point_num > 0.9:
+        time_lable = {str(float('%.3f'%(float(key)/rate))): softmax(opf_whole_pred[key]).tolist()[0] for key in
+                      list(rgb_whole_pred.keys())}
+        fig_y = fig_y_opf.copy()
+    elif zero_count / point_num > 0.9:
+        time_lable = {str(float('%.3f' % (float(key) / rate))): softmax(rgb_whole_pred[key]).tolist()[0] for key in
+                      list(rgb_whole_pred.keys())}
+        fig_y = fig_y_rgb.copy()
 
     fig_x_1 = fig_x[:1]
     fig_y_1 = fig_y[:1]
     fig_y_rgb_1 = fig_y_rgb[:1]
     fig_y_opf_1 = fig_y_opf[:1]
-    
+
     for i in range(1, len(fig_x)):
-        fig_x_1.append(fig_x[i]-0.001)
+        fig_x_1.append(fig_x[i] - 0.001)
         fig_x_1.append(fig_x[i])
         fig_y_1.append(fig_y_1[-1])
         fig_y_1.append(fig_y[i])
@@ -273,32 +310,10 @@ if __name__ == '__main__':
     fig_y_1.append(fig_y[-1])
     fig_y_rgb_1.append(fig_y_rgb[-1])
     fig_y_opf_1.append(fig_y_opf[-1])
-    # plt.figure()
-    # plt.plot(fig_x_1, fig_y_1, linewidth=2, color='lightskyblue')
-    # plt.xlabel('time/s')
-    # plt.ylabel('Slipping probability')
-    # plt.ylim(0, 1)
-    # plt.xlim(0, duration + 0.2)
-    # plt.title('two stream network')
-    # plt.savefig('bucket1/' + video_title + 'timeLable.jpg')
-    # plt.show()
 
-    # plt.figure()
-    # plt.plot(fig_x_1, fig_y_rgb_1, linewidth=2, color='skyblue')
-    # plt.xlabel('time/s')
-    # plt.ylabel('Slipping probability')
-    # plt.ylim(0, 1)
-    # plt.xlim(0, duration + 0.2)
-    # plt.title('spatial stream network')
-    # plt.savefig('bucket1/' + video_title + '_rgb_' + 'timeLable.jpg')
-    # plt.show()
-
-    plt.figure()
-    plt.plot(fig_x_1, fig_y_opf_1, linewidth=2, color='blue')
-    plt.xlabel('time/s')
-    plt.ylabel('Slipping probability')
-    plt.ylim(0, 1)
-    plt.xlim(0, duration + 0.2)
-    plt.title('motion stream network')
-    plt.savefig(video_title + '_opt_' + 'timeLable.jpg')
-    plt.show()
+    save_fig(fig_x_1, fig_y_1, 'two stream network', 'bucket3/' + video_title + '_Part6.jpg')
+    save_fig(fig_x_1, fig_y_rgb_1, 'spatial stream network', 'bucket3/' + video_title + '_rgb' + '_Part6.jpg')
+    save_fig(fig_x_1, fig_y_opf_1, 'motion stream network', 'bucket3/' + video_title + '_opf' + '_Part6.jpg')
+    rgb_whole_pred = {}
+    opf_whole_pred = {}
+    shutil.rmtree('record/')
